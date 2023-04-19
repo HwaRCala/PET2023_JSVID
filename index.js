d4886fa78f1fd58cc7e1c3df179c53841243db30
HwaRCala <leo.lviola@gmail.com>
2023-04-18 22:33:22 -0300

    Ejercicio 1

---
+++ index.js
@@ -0,0 +1,6 @@
+let num1 = 0;
+let num2 = 0;
+num1 = prompt("Ingrese un numero: A - a sumar");
+num2 = prompt("Ingrese un numero: B - a sumar");
+resultado = Number(num1) + Number(num2);
+alert(resultado);
