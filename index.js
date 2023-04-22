1d5575e4944551144a055425ae950550609c82a3
HwaRCala <leo.lviola@gmail.com>
2023-04-22 14:03:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = 5;
+let num2 = 3;
 num1 = prompt("Ingrese un numero: A - a sumar");
 num2 = prompt("Ingrese un numero: B - a sumar");
 resultado = Number(num1) + Number(num2);
