fae834ca9dcc9620d2b6f22e5dd2985cf41141ea
HwaRCala <leo.lviola@gmail.com>
2023-04-23 19:04:09 -0300

    Tres

--- Ejercicio 2/index.js
+++ Ejercicio 2/index.js
@@ -1,9 +1,9 @@
 let numeros = [
-    num1 = prompt("Ingrese primer numero a ordenar"),
-    num2 = prompt("Ingrese segundo numero a ordenar"),
-    num3 = prompt("Ingrese tercer numero a ordenar"),
-    num4 = prompt("Ingrese cuarto numero a ordenar"),
-    num5 = prompt("Ingrese quiinto numero a ordenar"),]
+    num1 = Number(prompt("Ingrese primer numero a ordenar")),
+    num2 = Number(prompt("Ingrese segundo numero a ordenar")),
+    num3 = Number(prompt("Ingrese tercer numero a ordenar")),
+    num4 = Number(prompt("Ingrese cuarto numero a ordenar")),
+    num5 = Number(prompt("Ingrese quiinto numero a ordenar")),]
 
 numeros.sort( function( a , b){
     if(a > b) return 1;
